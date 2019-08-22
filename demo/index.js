034637cc83c8ab3c38b8fee3b23f8fcb21fcc22c
Richter, Daniel (AU - Sydney) <darichter@deloitte.com.au>
2019-08-22 16:54:02 +1000

    updated demo

--- demo/index.js
+++ demo/index.js
@@ -1,21 +1,27 @@
-import { single } from '../src/index';
+import { single, list } from '../src/index';
 
 const elementRandom = document.getElementById('random-name-here');
 const elementMale = document.getElementById('male-name-here');
 const elementFemale = document.getElementById('female-name-here');
+const elementRandomList = document.getElementById('random-list-here');
 
 document.getElementById("random-name").addEventListener("click", function() {
-    elementRandom.innerHTML = single();
+    elementRandom.innerHTML = `single() > '${single()}'`;
 });
 
 document.getElementById("random-male").addEventListener("click", function() {
-    elementMale.innerHTML = single('MALE');
+    elementMale.innerHTML = `single('MALE') > '${single('MALE')}'`;
 });
 
 document.getElementById("random-female").addEventListener("click", function() {
-    elementFemale.innerHTML = single('FEMALE');
+    elementFemale.innerHTML = `single('FEMALE') > '${single('FEMALE')}'`;
 });
 
-elementRandom.innerHTML = single();
-elementMale.innerHTML = single('MALE');
-elementFemale.innerHTML = single('FEMALE');
+document.getElementById("random-list").addEventListener("click", function() {
+    elementRandomList.innerHTML = `list(5) > [${list(5)}]`;
+});
+
+elementRandom.innerHTML = `single() > '${single()}'`;
+elementMale.innerHTML = `single('MALE') > '${single('MALE')}'`;
+elementFemale.innerHTML = `single('FEMALE') > '${single('FEMALE')}'`;
+elementRandomList.innerHTML = `list(5) > [${list(5)}]`;
