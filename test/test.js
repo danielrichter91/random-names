0525ece8555bcb4fb8888cafc194665c539dd735
Richter, Daniel (AU - Sydney) <darichter@deloitte.com.au>
2019-08-23 11:41:07 +1000

    add test framework

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var randomNames = require('../index');
+var expect = require('chai').expect;
+
+describe('mock test', function() {
+    it('mock test' , function() {
+        expect(true);
+    });
+});
